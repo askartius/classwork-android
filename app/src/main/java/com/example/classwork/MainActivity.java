8c7c93ea579434fec2a16fffd84340e12c7e5ba1
Askartius <151680098+askartius@users.noreply.github.com>
2023-12-26 21:56:45 +0300

    Add data transfer between contexts

--- app/src/main/java/com/example/classwork/MainActivity.java
+++ app/src/main/java/com/example/classwork/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.classwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import com.example.classwork.databinding.ActivityMainBinding;
+import com.example.classwork.thread.Example;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private UUID id;
     public static final String TAG = "MainActivity";
 
     @Override
@@ -31,6 +40,47 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 Log.d(TAG, "Work has ended");
+            }
+        });
+
+        binding.doButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Data data = new Data.Builder()
+                        .putString("keyA", "value1")
+                        .putInt("keyB", 25)
+                        .build();
+
+                OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(Example.class)
+                        .setInputData(data)
+                        .build();
+
+                id = oneTimeWorkRequest.getId();
+
+                WorkManager
+                        .getInstance(getApplicationContext())
+                        .enqueue(oneTimeWorkRequest);
+            }
+        });
+
+        binding.getDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (id != null) {
+                    WorkManager.getInstance(getApplicationContext())
+                            .getWorkInfoByIdLiveData(id)
+                            .observe(MainActivity.this, new Observer<WorkInfo>() {
+                                @Override
+                                public void onChanged(WorkInfo workInfo) {
+                                    if (workInfo != null) {
+                                        Log.d(TAG, "WorkInfo state: " + workInfo.getState());
+
+                                        String stringData = workInfo.getOutputData().getString("keyC");
+                                        Log.d(TAG, "Result data: " + stringData);
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
--- app/src/main/java/com/example/classwork/thread/Example.java
+++ app/src/main/java/com/example/classwork/thread/Example.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -23,15 +24,24 @@ public class Example extends Worker {
             for (int i = 0; i < 3; i++) {
                 Log.d(TAG, "Doing...");
 
-                Thread.sleep(10 * 1000);
+                Data inputData = getInputData();
+                String stringData = inputData.getString("keyA");
+                int intData = inputData.getInt("keyB", 0);
 
+                Log.d(TAG, "Data: " + stringData + ", " + intData);
+
+                Thread.sleep(10 * 1000);
             }
         } catch (InterruptedException e) {
             Log.e(TAG, e.getMessage());
             return Result.failure();
         }
 
+        Data data = new Data.Builder()
+                .putString("keyC", "Hello from Example")
+                .build();
+
         Log.d(TAG, "Work done");
-        return Result.success();
+        return Result.success(data);
     }
 }
