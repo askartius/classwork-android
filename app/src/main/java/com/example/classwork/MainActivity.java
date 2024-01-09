039555fc0927bea1a3155cda31945507406a2e76
Askartius <151680098+askartius@users.noreply.github.com>
2024-01-09 20:54:18 +0300

    Fragments

--- app/src/main/java/com/example/classwork/MainActivity.java
+++ app/src/main/java/com/example/classwork/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.classwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.example.classwork.databinding.ActivityMainBinding;
+import com.example.classwork.fragment.Fragment1;
+import com.example.classwork.fragment.Fragment2;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    public static final String TAG = "MainActivity";
+    private FragmentManager fragmentManager;
+    private Fragment fragment1;
+    private Fragment2 fragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +25,27 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.startButton.setOnClickListener(new View.OnClickListener() {
+        fragmentManager = getSupportFragmentManager();
+        fragment1 = (Fragment1) fragmentManager.getFragments().get(0);
+        fragment2 = new Fragment2();
+
+        binding.button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Work in progress");
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment1);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
 
-                try {
-                    Thread.sleep(10 * 1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
-                Log.d(TAG, "Work has ended");
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment2)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
     }
